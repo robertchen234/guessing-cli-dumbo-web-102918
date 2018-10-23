69d5031a928053f91b898771842f431806f73c05
Robert Chen <robertchen234@gmail.com>
2018-10-23 00:31:32 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   input = gets.chomp 
   if input == "exit"
     puts "Goodbye!"
-  elsif input == num
+  elsif input.to_i == num
     puts "You guessed the correct number!"
   end
 end 
